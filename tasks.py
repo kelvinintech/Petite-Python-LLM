bb034e3382e31090587151d437576080195d0a66
Kelvin R. Tobias <110644520+kelvinintech@users.noreply.github.com>
2024-10-09 03:14:48 +0000

    started creating comments for steps to create petite llm

--- tasks.py
+++ tasks.py
@@ -1,1 +1,8 @@
-#completed 
+#steps for creating petitie, the python llm 
+#created repository for petitle
+#created codespaces
+#verified python installation
+#installed docker with these commands
+# sudo apt-get install
+#sudo apt-get docker.io
+#set up Jenkins using Docker
