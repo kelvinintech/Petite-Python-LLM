9ad79d31308c3495da12ef5986a502b226f673a2
Kelvin R. Tobias <110644520+kelvinintech@users.noreply.github.com>
2024-10-11 23:01:26 +0000

    started training llm, installed tensorflow, jenkins dashboard created

---
+++ learning_language_model/model.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+class LearningLanguageModel:
+    def __init__(self):
+        pass 
+#helps your llm learn
+
+    def train(self, training_data):
+        pass
+
+    def evaluate(self, test_data):
+        pass
